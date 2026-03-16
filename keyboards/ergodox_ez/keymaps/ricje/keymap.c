c43d831233dab68de0c7de7ffc7df2e789dcc9c0
Jean-Francois Richard <ricje@me.com>
2026-03-16 11:49:12 -0400

    Increase S and L tapping term

--- keyboards/ergodox_ez/keymaps/ricje/keymap.c
+++ keyboards/ergodox_ez/keymaps/ricje/keymap.c
@@ -129,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     if (is_alt_home_row_mod(keycode)) {
-        return 140;
+        return 170;
     }
 
     if (is_home_row_mod(keycode)) {
